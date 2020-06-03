38187fd33ca1d48b7202bc17dd5f286fb4d0705c
Nati <natan.kamusher@gmail.com>
2020-06-03 17:35:08 +0300

    Working on the modal show/hide functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,16 @@
 let ticking = false; // represent continus scroll
 let navCheckbox;
+let btnModalOpen, btnModalClose;
+
 document.addEventListener('DOMContentLoaded', () => {
     navCheckbox = document.querySelector('.navbar__toggle input[type="checkbox"]');
+    btnModalShow = document.querySelectorAll('.js-modal-show');
+    btnModalHide = docuemnt.querySelectorAll('.js-modal-hide');
     
     navCheckbox.addEventListener('click', navToggle);
+    btnModalShow.forEach(btn => {
+        btn.addEventListener('click', showModal)
+    })
 });
 
 // The purpose of this function is to hide the mobile nav upon scroll
@@ -20,4 +27,14 @@ function onScroll() {
     if(window.scrollY >= 300) {
         navCheckbox.checked = false;
     } 
+}
+
+function showModal(element) {
+    element.classList.remove('card__modal--visible');
+    element.classList.add('card__modal--visible');
+}
+
+function closeModal(element) {
+    element.classList.remove('card__modal--visible')
+    element.classList.add('card__modal--hidden')
 }
